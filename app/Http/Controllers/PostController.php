5c1082dd47941f3ca61e896b35e5912b521f1a4a
Morteza <mor.zakeri@gmail.com>
2019-08-04 09:37:04 +0200

    11-Was-it-validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,16 +14,25 @@ class PostController extends Controller {
     }
 
     public function store(Request $request) {
+
+        $this->validate($request, [
+            'body' => 'required|string|min:3|max:512',
+            'title' => 'required|string|min:2|max:128'
+        ]);
         $created = Post::create($request->input());
         return response()->json($created, 201);
     }
 
     public function update(Request $request) {
+        $this->validate($request, [
+            'post_id' => 'required|integer|exists:posts,id',
+            'body' => 'required|string|min:3|max:512',
+        ]);
         $postId = $request['post_id'];
         $body = $request['body'];
 
-        $updated = Post::where('id',$postId)->update([
-            'body'=>$body
+        $updated = Post::where('id', $postId)->update([
+            'body' => $body
         ]);
         return response()->json($updated, 201);
     }
