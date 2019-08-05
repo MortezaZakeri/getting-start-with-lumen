2d822b99e672a2ba818a42c40f8ca2a97aa6c3cd
Morteza <mor.zakeri@gmail.com>
2019-08-05 21:05:03 +0200

    13-We-need-user-model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,4 +16,8 @@ class Post extends Model {
         'title',
         'body'
     ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2019_08_04_065102_create_posts_table.php
+++ database/migrations/2019_08_04_065102_create_posts_table.php
@@ -13,6 +13,7 @@ class CreatePostsTable extends Migration {
     public function up() {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
             $table->string('title');
             $table->text('body');
             $table->unsignedTinyInteger('enable')->default(1);
